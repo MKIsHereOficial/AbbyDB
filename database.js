f853d283a5d299431c512657ef04f750aacfc023
Minna <>
2021-04-22 03:45:16 +0000

    API Básica com GET.

--- database.js
+++ database.js
@@ -26,11 +26,134 @@ function initializeDatabase(databaseName = "default".toLowerCase()) {
   return databaseInfo;
 }
 /////////////////////////////////////////////////////////////////////////
-function main(databaseName = "default".toLowerCase()) {
+async function main(databaseName = "default".toLowerCase()) {
   console.log(`Database iniciada como ${databaseName}`);
 
-  initializeDatabase(databaseName);
+  const database = initializeDatabase(databaseName);
+  database.path = path.join(databasesPath, databaseName);
 
+  async function set(key = "", value = null) {
+    if (!key) return new Error("O Documento necessita de uma key/nome para ser criado/editado.");
+
+    const keyPath = path.join(database.path, `${key}.json`);
+
+    const docRef = (await fs.existsSync(keyPath)) ? await fs.readFileSync(keyPath, 'utf-8') : false;
+
+    const obj = {key, path: keyPath};
+    
+    if (!docRef && typeof value != undefined) {
+      console.log(`O Documento é inexistente, devido à um valor ter sido passado, ele foi criado.`);
+      await fs.writeFileSync(keyPath, JSON.stringify({key, value}, null, 1));
+    } else if(typeof value === undefined) {
+      await fs.writeFileSync(keyPath, JSON.stringify({key, value}, null, 1));
+    } else if (!docRef) {
+      obj.err = new Error("Este Documento/Key é inexistente ou um valor não foi definido, por isso não foi retornado.");
+    }
+
+    obj.value = value;
+    if (await fs.existsSync(keyPath)) {
+      await fs.writeFileSync(keyPath, JSON.stringify({key, value}, null, 1));
+    }
+    
+    return obj;
+  }
+
+  async function del(key = "") {
+    const keyPath = path.join(database.path, `${key}.json`);
+
+    if (await fs.existsSync(keyPath)) {
+      await fs.unlinkSync(keyPath);
+    } else {
+      return {err: new Error("O Documento/Key não existe, então não pode ser excluído.")};
+    }
+
+    return {key, excluded: true};
+  }
+
+  async function get(key = "", fallback) {
+    const keyPath = path.join(database.path, `${key}.json`);
+
+    const docRef = (await fs.existsSync(keyPath)) ? await fs.readFileSync(keyPath, 'utf-8') : false;
+    const doc = (docRef) ? JSON.parse(docRef) : false;
+
+    const obj = {key, path: keyPath};
+
+    if (doc) {
+      obj.value = doc.value;
+    } else {
+      obj.err = new Error("Não foi possível encontrar os valores do Documento.");
+      (fallback) ? obj.value = fallback : null;
+    }
+
+    return obj;
+  }
+
+  async function add(key = "", value = 1) {
+    if (!key) return new Error("O Documento necessita de uma key/nome para ser criado/editado.");
+
+    const keyPath = path.join(database.path, `${key}.json`);
+
+    const docRef = (await fs.existsSync(keyPath)) ? await fs.readFileSync(keyPath, 'utf-8') : false;
+    const doc = (docRef) ? JSON.parse(docRef) : false;
+
+    const obj = {key, path: keyPath};
+
+    if (doc && !isNaN(doc.value)) {
+      obj.value = doc.value;
+    } else if (doc && isNaN(doc.value)) {
+      obj.err = new Error("O valor do Documento não é um número (nem int, nem float). Para adicionar, é necessário que o valor seja um número");
+    } else {
+      obj.err = new Error("O valor do Documento/Key não pode ser encontrado.");
+    }
+
+    if (value && !isNaN(obj.value)) {
+      obj.value = obj.value + value;
+      await set(key, obj.value);
+    }
+
+    return obj;
+  }
+
+  async function exists(key = "") {
+    if (!key || !key.length) return new Error("Preciso de uma Chave para poder encontrar o Documento.");
+
+    const keyPath = path.join(database.path, `${key}.json`);
+
+    var obj = {key, path: keyPath};
+
+    if (await fs.existsSync(keyPath)) {
+      obj.exists = true;
+      obj = await get(key, true);
+    } else {
+      obj.exists = false;
+    }
+
+    return obj;
+  }
+
+  async function all() {
+    const keys = [];
+
+    const files = await fs.readdirSync(database.path);
+    files.map(file => {
+      const key = file.replace(".json", "");
+      keys.push(key);
+    })
+
+    const realKeys = await keys.map(async key => {
+      var obj = {};
+
+      await get(key).then(data => {obj = data});
+
+      if (!obj) return key;
+      
+      return obj;
+    });
+
+    return realKeys.map(data => {return data;});
+  }
+
+  return {database, set, delete: del, del, get, fetch: get, add, exists, getAll: all};
 }
 
 module.exports = main;
--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const express = require('express'), app = express();
 const APP_PORT = process.env.PORT || 3000;
 
-const db = require('./database.js');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send(db());
+const Database = require('./database.js');
+
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
 });
+
+app.get('/database', async (req, res) => {
+  const dbName = (req.params && req.params['database']) ? req.params['database'] : "default";
+  res.json(await Database(dbName));
+});
+app.get('/database/:database', async (req, res) => {
+  const {database} = req.params;
+  const db = await Database(database);
+
+  res.json(db);
+});
+app.get('/database/:database/all', async (req, res) => {
+  const {database} = req.params;
+  const db = await Database(database);
+
+  console.dir(await db.getAll());
+
+  res.json({all: await db.getAll(), db});
+});
+app.get('/database/:database/:key', async (req, res) => {
+  const {database, key} = req.params;
+  const db = await Database(database);
+
+  var obj = await db.exists(key);
+  obj = (obj.exists != false) ? await db.get(key) : false;
+
+  if (!obj) obj = await db.set(key, key);
+
+  res.json(obj);
+});
+app.get('/database/:database/:key/set', async (req, res) => {
+  res.json({err: new Error("Não é possível dar GET nessa página.").message})
+});
+/*app.post('/database/:database/:key/set', async (req, res) => {
+  const {database, key} = req.params;
+  const db = await Database(database);
+
+  console.log(req);
+
+  var obj = await db.set(key, false);
+
+  res.json(obj);
+});*/
 
 app.listen(APP_PORT || 3000, () => {
   console.log(`Servidor iniciado na porta ${APP_PORT}`);
